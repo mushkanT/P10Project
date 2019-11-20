f8de2de77fb814b422f3785b70a072afd3ebd0e6
Patrick Alminde <palminde@hotmail.com>
2019-11-20 09:52:48 +0100

    Ready for usage - Inception is untested

--- Evaluation.py
+++ Evaluation.py
@@ -8,7 +8,7 @@ from precision_recall import knn_precision_recall_features
 
 
 def init_tf(random_seed=1234):
-    """Initialize TF."""
+    """Initialize TF"""
     print('Initializing TensorFlow...\n')
     np.random.seed(random_seed)
     tfutil.init_tf({'graph_options.place_pruned_graph': True,
@@ -31,9 +31,17 @@ def initialize_feature_extractor_incept():
     return net
 
 
-def evaluate(real_images, generated_images, batch_size, feature_model=0):
+def evaluate(real_images, generated_images, batch_size=100, feature_model=0):
+    """
+    Evaluates precision and recall of estimated manifold on two sets of image samples
+    :param real_images: Tensor of real image samples NCHW
+    :param generated_images: Tensor of generated image samples NCHW
+    :param batch_size: Number of features calculated at a time (limited by GPU ram)
+    :param feature_model: Determines whether to use VGG16 (arg 0) or inceptionV3(arg 1)
+    :return: state: Dictionary containing precision and recall metrics
+    """
     # Ensures that same amount of images in both samples
-    assert(real_images.shape(0) == generated_images.shape(0))
+    assert(real_images.shape[1] == generated_images.shape[1])
     assert(len(real_images.shape) == len(generated_images.shape))
 
     init_tf()
@@ -46,16 +54,19 @@ def evaluate(real_images, generated_images, batch_size, feature_model=0):
         feature_net = initialize_feature_extractor_incept()
 
     # Calculate features vectors for real image samples
+    print('Calculating feature vectors for real images...')
     ref_features = np.zeros([num_images, feature_net.output.shape[1]], dtype=np.float32)
     for begin in range(0, num_images, batch_size):
         end = min(begin + batch_size, num_images)
         ref_features[begin:end] = feature_net.run(real_images[begin:end], num_gpus=1, assume_frozen=True)
 
     # Calculate feature vectors for generated image samples
+    print('Calculating feature vector for generated images...')
     eval_features = np.zeros([num_images, feature_net.output.shape[1]], dtype=np.float32)
     for begin in range(0, num_images, batch_size):
         end = min(begin+batch_size, num_images)
         eval_features[begin:end] = feature_net.run(generated_images[begin:end], num_gpus=1, assume_froze=True)
 
+    print('Estimating manifold of feature vectors and calculating precision/recall...')
     state = knn_precision_recall_features(ref_features, eval_features)
     return state
--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import os
 
 import VAE_Usage
+import Evaluation
 
 VAE_Usage.VAE_CIFAR('0001', 'run/')
 # VAE_Usage.VAE_MNIST('0001', 'run/')
