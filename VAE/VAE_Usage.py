78406fbf0f844f982a09f6efb86980ed3e91510f
Patrick Alminde <palminde@hotmail.com>
2019-12-12 15:09:29 +0100

    again

--- VAE/VAE_Usage.py
+++ VAE/VAE_Usage.py
@@ -44,7 +44,7 @@ def VAE_MNIST(RUN_ID, RUN_FOLDER, lr, r_loss, batch_size, epochs, print_n_batche
 
     VAE.compile(learning_rate=lr, r_loss_factor=r_loss)
 
-    (x_train, x_test) = datahandler.mnist(norm_setting=1)
+    (x_train, x_test) = datahandler.mnist(norm_setting=0)
 
     VAE.train(
         x_train,
@@ -94,7 +94,7 @@ def VAE_CIFAR(RUN_ID, RUN_FOLDER, lr, r_loss, batch_size, epochs, print_n_batche
 
     VAE.compile(learning_rate=lr, r_loss_factor=r_loss)
 
-    (x_train, x_test) = datahandler.cifar10(norm_setting=1)
+    (x_train, x_test) = datahandler.cifar10(norm_setting=0)
 
     VAE.train(
         x_train,
