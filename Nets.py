0a5f5fb14b69dfada433c51d5a132d42eadd98ea
Patrick Alminde <palminde@hotmail.com>
2020-05-06 13:56:31 +0200

    glemte at fjerne 1 sigmoid -.-

--- Nets.py
+++ Nets.py
@@ -489,7 +489,7 @@ def cogan_discriminators_digit_noshare(args):
     model1 = tf.keras.layers.Flatten()(model1)
     model1 = tf.keras.layers.Dense(500)(model1)
     model1 = tf.keras.layers.PReLU()(model1)
-    model1 = tf.keras.layers.Dense(1, activation='sigmoid')(model1)
+    model1 = tf.keras.layers.Dense(1)(model1)
 
     # Discriminator 2
     img2 = tf.keras.layers.Input(shape=img_shape)
